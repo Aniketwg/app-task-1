3187f9837dca42db0e0c7e0d6e496b8edb327b4f
root <root@ip-172-31-6-230.ap-south-1.compute.internal>
2023-01-16 11:11:07 +0000

    added a command.sh file

---
+++ command.sh
@@ -0,0 +1,11 @@
+#step1- yum install nodejs
+#step2- npm install creat-react-app
+#step3- create-react-app my-app
+#step4- cd my-app & git init, git remote add origin
+#step5- git branch update_logo
+#step6- git checkout update_logo
+#step7- Replce the existing  logo and link using text editor.
+#step8- git commit -m "update logo and link"
+#       git push origin update_logo.
+#
+#Git repo url = https://github.com/Aniketwg/app-task-1.git        
